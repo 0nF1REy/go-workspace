54d5628c8a4bfe998045a46518255fa50901999c
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-03-18 14:22:31 -0300

    main: feat (19_web_scraper/02_oxylabs/03_auto_pagination) - Added error handling and request timeout to auto-pagination example
    - Introduced error handling for HTTP responses and sets a request timeout, improving robustness and reliability of the auto-pagination web scraper example.

--- src/cmd/examples/19_web_scraper/02_oxylabs/03_auto_pagination/main.go
+++ src/cmd/examples/19_web_scraper/02_oxylabs/03_auto_pagination/main.go
@@ -5,6 +5,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -12,6 +14,8 @@ import (
 func main() {
 
 	c := colly.NewCollector()
+
+	c.SetRequestTimeout(10 * time.Second)
 
 	// Quando encontrar o botão "next", visita automaticamente a próxima página
 	c.OnHTML("li.next > a", func(e *colly.HTMLElement) {
@@ -23,8 +27,29 @@ func main() {
 		e.Request.Visit(nextPageURL)
 	})
 
+	// Tratamento de erro
+	c.OnError(func(r *colly.Response, err error) {
+		switch r.StatusCode {
+
+		case http.StatusNotFound:
+			fmt.Println("Erro 404: Página não encontrada:", r.Request.URL)
+
+		case http.StatusTooManyRequests:
+			fmt.Println("Erro 429: Muitas requisições (rate limit atingido). Tente novamente mais tarde:", r.Request.URL)
+
+		case http.StatusInternalServerError:
+			fmt.Println("Erro 500: Erro interno do servidor:", r.Request.URL)
+
+		default:
+			fmt.Printf("Erro %d ao acessar %s: %v\n", r.StatusCode, r.Request.URL, err)
+		}
+	})
+
+	// url := "https://sandbox.oxylabs.io/products/non-existent-page"
 	url := "https://sandbox.oxylabs.io/products"
+
 	err := c.Visit(url)
+
 	if err != nil {
 		log.Fatal(err)
 	}
