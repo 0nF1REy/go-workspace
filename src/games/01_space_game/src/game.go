678496a3a784f3dac9a3b89c999f319b5a815133
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-03-19 11:33:02 -0300

    main: refactor (games/01_space_game) - Enhanced player movement logic in Update method and added comments for clarity

--- src/games/01_space_game/src/game.go
+++ src/games/01_space_game/src/game.go
@@ -13,12 +13,16 @@ func NewGame() *Game {
 	}
 }
 
+// Roda em 60 FPS
 // Responsável por atualizar a lógica do jogo
+// 60 X por segundo
 func (g *Game) Update() error {
+	g.player.Update()
 	return nil
 }
 
 // Responsável por desenhar objetos na tela
+// 60 X por segundo
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
 }
