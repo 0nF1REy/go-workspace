f5eb28ace2e8974f8c60af2bf9e4a9bc12d4e871
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-03-19 13:08:06 -0300

    main: feat (games/01_space_game) - Added collision detection between lasers and meteors in Update method

--- src/games/01_space_game/src/game.go
+++ src/games/01_space_game/src/game.go
@@ -52,6 +52,15 @@ func (g *Game) Update() error {
 		}
 	}
 
+	for i, m := range g.meteors {
+		for j, l := range g.lasers {
+			if m.Collider().Intersects(l.Collider()) {
+				g.meteors = append(g.meteors[:i], g.meteors[i+1:]...)
+				g.lasers = append(g.lasers[:j], g.lasers[j+1:]...)
+			}
+		}
+	}
+
 	return nil
 }
 
