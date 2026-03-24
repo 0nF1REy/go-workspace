67c40dfe0415dbe37fce4418b0968b79bb6db31a
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-03-24 13:28:03 -0300

    main: feat (cmd/examples/21_expense_tracker) - Added expense tracker CLI example
    - Introduced a Go-based CLI for managing financial transactions, supporting income and expenses, listing, and CSV export.

---
+++ src/cmd/examples/21_expense_tracker/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Transaction struct {
+	ID       int
+	Amount   float64
+	Category string
+	Date     time.Time
+	Type     string
+}
+
+type BudgetTracker struct {
+	transactions []Transaction
+	nextID       int
+}
+
+type FinancialRecord interface {
+	GetAmount() float64
+	GetType() string
+}
+
+func (t Transaction) GetAmount() float64 {
+	return t.Amount
+}
+
+func (t Transaction) GetType() string {
+	return t.Type
+}
+
+func (bt *BudgetTracker) AddTransaction(amount float64, category, tType string) {
+	newTransaction := Transaction{
+		ID:       bt.nextID,
+		Amount:   amount,
+		Category: category,
+		Date:     time.Now(),
+		Type:     tType,
+	}
+	bt.transactions = append(bt.transactions, newTransaction)
+	bt.nextID++
+}
+
+func (bt BudgetTracker) DisplayTransactions() {
+	fmt.Println("ID\tValor\tCategoria\tData\tTipo")
+	for _, transaction := range bt.transactions {
+		fmt.Printf("%d\t%.2f\t%s\t%s\t%s\n",
+			transaction.ID,
+			transaction.Amount,
+			transaction.Category,
+			transaction.Date.Format("02/01/2006 15:04"),
+			transaction.Type,
+		)
+	}
+}
+
+func (bt BudgetTracker) CalculateTotal(tType string) float64 {
+	var total float64
+	for _, transaction := range bt.transactions {
+		if transaction.Type == tType {
+			total += transaction.Amount
+		}
+	}
+	return total
+}
+
+func (bt BudgetTracker) SaveToCSV(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"ID", "Valor", "Categoria", "Data", "Tipo"})
+
+	for _, t := range bt.transactions {
+		record := []string{
+			strconv.Itoa(t.ID),
+			strconv.FormatFloat(t.Amount, 'f', 2, 64),
+			t.Category,
+			t.Date.Format("02/01/2006 15:04"),
+			t.Type,
+		}
+		writer.Write(record)
+	}
+	fmt.Println("Transações salvas em", filename)
+	return nil
+}
+
+func main() {
+	// bt := BudgetTracker()
+	for {
+		fmt.Println("\n=== Controle Financeiro ===")
+		fmt.Println("1. Adicionar transação")
+		fmt.Println("2. Listar transações")
+		fmt.Println("3. Total de receitas")
+		fmt.Println("4. Total de despesas")
+		fmt.Println("5. Salvar em CSV")
+		fmt.Println("6. Sair")
+		fmt.Println("Escolha uma opção:")
+	}
+}
